2efec773bce51ca13b3c2366507bee102adf63f8
arthas1888 <tavomorales88@gmail.com>
2016-11-19 11:31:01 -0500

    fragmentos en Android

--- FragmentApplication/app/src/main/java/com/example/a68/fragmentapplication/MainActivity.java
+++ FragmentApplication/app/src/main/java/com/example/a68/fragmentapplication/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.a68.fragmentapplication;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        replaceFragment(new MainFragment());
+
+    }
+
+    public void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.container, fragment);
+        fragmentTransaction.commit();
     }
 }
---
+++ FragmentApplication/app/src/main/java/com/example/a68/fragmentapplication/MainFragment.java
@@ -0,0 +1,35 @@
+package com.example.a68.fragmentapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * Created by 68 on 19/11/2016.
+ **/
+
+public class MainFragment extends Fragment implements View.OnClickListener{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.main_fragment, container, false);
+        Button button = (Button) view.findViewById(R.id.button);
+        button.setOnClickListener(this);
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button:
+                break;
+            default:
+                break;
+        }
+    }
+}
