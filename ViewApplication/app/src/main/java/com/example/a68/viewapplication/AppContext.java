44b14703235933da35f3c8772935e3dbd6f86c48
arthas1888 <tavomorales88@gmail.com>
2016-12-03 11:55:20 -0500

    proyecto vistas y estilos

---
+++ ViewApplication/app/src/main/java/com/example/a68/viewapplication/AppContext.java
@@ -0,0 +1,15 @@
+package com.example.a68.viewapplication;
+
+import android.app.Application;
+
+/**
+ * Created by 68 on 03/12/2016.
+ */
+public class AppContext extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
